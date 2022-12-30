dffb15078f277bbcea22af2d59d3e269ff984349
Mikl <66132000+MCNazarov@users.noreply.github.com>
2022-12-30 21:48:08 +0300

    Create my-config.tf

---
+++ my-config.tf
@@ -0,0 +1,15 @@
+terraform {
+   required_providers {
+     yandex = {
+       source = "yandex-cloud/yandex"
+     }
+   }
+ }
+  
+ provider "yandex" {
+   token  =  "<OAuth-токен>"
+   cloud_id  = "<идентификатор_облака>"
+   folder_id = "<идентификатор_каталога>"
+   zone      = "<зона_доступности_по_умолчанию>"
+ }
+  
